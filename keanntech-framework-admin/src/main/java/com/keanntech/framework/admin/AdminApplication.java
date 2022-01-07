742abf20df26794f75f1f88c37615b33065c35e5
keanntech <keanntech@aliyun.com>
2022-01-07 15:50:11 +0800

    初始化,可正常启动项目；

---
+++ keanntech-framework-admin/src/main/java/com/keanntech/framework/admin/AdminApplication.java
@@ -0,0 +1,17 @@
+package com.keanntech.framework.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author miaoqingfu
+ * @date 2022年01月07日 3:05 下午
+ */
+@SpringBootApplication
+public class AdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+
+}
